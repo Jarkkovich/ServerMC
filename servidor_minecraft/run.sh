4998dfcf27fb34d857b3b51db86ede2771097e9b
Jarkkovich <127717841+Jarkkovich@users.noreply.github.com>
2024-07-30 15:40:29 +0000

    Pending changes exported from your codespace

--- servidor_minecraft/run.sh
+++ servidor_minecraft/run.sh
@@ -3,4 +3,5 @@
 # Add custom JVM arguments to the user_jvm_args.txt
 # Add custom program arguments {such as nogui} to this file in the next line before the "$@" or
 #  pass them to this script directly
-java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.0.114-beta/unix_args.txt "$@"
+playit &
+java @user_jvm_args.txt @libraries/net/neoforged/neoforge/21.0.130-beta/unix_args.txt "$@"
